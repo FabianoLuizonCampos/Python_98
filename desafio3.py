72d6054f6eb09f24faae135855c425cdce65bb22
Fabiano Luizon Campos <65176857+FabianoLuizonCampos@users.noreply.github.com>
2024-03-14 11:32:36 -0300

    Update desafio3.py
    Atualizei nome

--- desafio3.py
+++ desafio3.py
@@ -1,7 +1,7 @@
 lista_de_lanches = ["BIG MAC", "MC CHEDDAR", "QUARTEIRAO", "BIG TASTY"]
 lista_de_precos = [ 29.90, 27.90, 18.50, 35.60]
 
-print("---- BEM VINDO AO MAC SENAI 164 -----")
+print("---- BEM VINDO AO MAC SENAI Maua -----")
 opcao = int(input("Digite sua opcao de lanche:"))
 
 print("Seu lanche escolhido foi ",lista_de_lanches[opcao - 1])
